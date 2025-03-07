f2a8c0c74c2c4d3c854caad758111fae3cbafc1a
AbdullahAlTowsif <abdullah.al.towsif2002@gmail.com>
2025-03-07 23:42:08 +0600

    first migration done

--- book/views.py
+++ book/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
-
+from book.forms import BookStoreForm
+from book.models import BookStoreModel
 # Create your views here.
 
 def home(request):
     return render(request, 'store_book.html')
+
+def store_book(request):
+    if request.method == 'POST':
+        book = BookStoreForm(request.POST)
+        if book.is_valid():
+            book.save(commit=False)
+            print(book.cleaned_data)
+    else:
+        book = BookStoreForm()
+    return render(request, 'store_book.html', {'form': book})
+
+def show_books(request):
+    book = BookStoreModel.objects.all()
+    return render(request, 'show_book.html', {'data': book})
